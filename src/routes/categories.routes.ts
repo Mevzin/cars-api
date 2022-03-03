d66c0e8e47fbd80dc41880447d0377e7030a44c1
Thiago Torres de Souza <thiagomev@gmail.com>
2022-03-03 05:25:16 -0300

    Categories route ref

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,16 @@
 import { Router } from "express";
 
-import { CreateCategoryUseCase } from "../modules/cars/useCases/createCategory/CreateCategoryUseCase";
-
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategorys";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryUseCreateCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-
-  createCategoryUseCreateCategoryUseCase.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 })
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.json(all);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
