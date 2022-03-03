44411ead7ef3a9afeff42b3c7ba9a7a77a2146d7
Thiago Torres de Souza <thiagomev@gmail.com>
2022-03-03 10:58:48 -0300

    Add Specification Route and ref routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
-import { listCategoriesController } from "../modules/cars/useCases/listCategorys";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
